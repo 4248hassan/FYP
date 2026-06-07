808f932799c1c940005c7aa911a9894347365a8b
Hassan Khan <hassank4248@gmail.com>
2026-06-07 16:03:54 +0500

    solved issue

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,20 +1,3 @@
-// import { StrictMode } from 'react'
-// import { createRoot } from 'react-dom/client'
-// import { BrowserRouter } from 'react-router-dom'
-// import './index.css'
-// import AppRouter from './router/AppRouter.jsx'
-// import { AuthProvider } from './context/AuthContext.jsx'
-
-// createRoot(document.getElementById('root')).render(
-//   <StrictMode>
-//     <AuthProvider>
-//       <BrowserRouter basename="/resolveit-frontend">
-//         <AppRouter />
-//       </BrowserRouter>
-//     </AuthProvider>
-//   </StrictMode>,
-// )
-
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
@@ -25,9 +8,26 @@ import { AuthProvider } from './context/AuthContext.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <AuthProvider>
-      <BrowserRouter>
+      <BrowserRouter basename="/resolveit-frontend">
         <AppRouter />
       </BrowserRouter>
     </AuthProvider>
   </StrictMode>,
 )
+
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
+// import { BrowserRouter } from 'react-router-dom'
+// import './index.css'
+// import AppRouter from './router/AppRouter.jsx'
+// import { AuthProvider } from './context/AuthContext.jsx'
+
+// createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+//     <AuthProvider>
+//       <BrowserRouter>
+//         <AppRouter />
+//       </BrowserRouter>
+//     </AuthProvider>
+//   </StrictMode>,
+// )
